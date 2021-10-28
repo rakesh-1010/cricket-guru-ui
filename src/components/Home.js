946e90dfb280ffe96e8335e4eae5059f8ece25c7
Rajdeep Yadav <rajdeep.20.yadav@gmail.com>
2021-10-28 22:29:37 +0530

    Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 import UserService from "../services/user.service";
+import logo from '../assets/cc.jpeg'; // with import
+
+
 
 const Home = () => {
   const [content, setContent] = useState("");
@@ -24,7 +27,18 @@ const Home = () => {
   return (
     <div className="container">
       <header className="jumbotron">
-        <h3>{content}</h3>
+        <h3>Welcome to CricketGuru</h3>
+        <p>
+          The proposed Cricket Academy Management System
+          is completely automated. The proposed system allows
+          the admin and coaches to maintain various records,
+          register for the various training batches,Diet
+          Plan,Manage schedule,Player record and Coaches
+          player Ratings. Less effort is required for maintaining
+          the database of online cricket Academy Management
+          using this software.
+        </p>
+        <img src={logo} />
       </header>
     </div>
   );
